5bc13e91ef793a219a20b1360fc9c3247107cf26
Vincent-Baret1 <vincent.baret@etu.umontpellier.fr>
2022-01-19 08:53:49 +0100

    avancée sur Mon parcours

--- src/components/MonParcoursRectangle.js
+++ src/components/MonParcoursRectangle.js
@@ -8,11 +8,14 @@ class MonParcoursRectangle extends React.Component {
                 <h1 className="h1-Mon-Parcours">Mon parcours</h1>
                 
                 <div className="div-frise-chrono">
-                    <div><h1>2017</h1></div>
-                    <div><h1>2020</h1></div>
-                    <div><h1>2021</h1></div>
-                    <div><h1>2022</h1></div>
+                    <div id="div-annee-frise"><h1 id="h1-annee-frise">2017</h1></div>
+                    <div id="div-annee-frise"><h1 id="h1-annee-frise">2020</h1></div>
+                    <div id="div-annee-frise"><h1 id="h1-annee-frise">2021</h1></div>
+                    <div id="div-annee-frise"><h1 id="h1-annee-frise">2022</h1></div>
                 </div>
+
+                <div className="ligne-2017"></div>
+              
             </div>
         );
     }
