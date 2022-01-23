938cc387db478e728d886ce8f41206cce0f76ec7
Vincent-Baret1 <vincent.baret@etu.umontpellier.fr>
2022-01-23 19:45:55 +0100

    suppression possibilité thème

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
 import './App.css';
-import { lightTheme, darkTheme } from './theme';
-import { GlobalStyles } from './global';
-import { ThemeProvider } from "styled-components";
 import TopRectangle from './components/TopRectangle';
-import { Button } from 'react-bootstrap';
 import MonParcoursRectangle from './components/MonParcoursRectangle';
 import ExperienceProRectangle from './components/ExperienceProRectangle';
 import MesProjetsRectangle from './components/MesProjetsRectangle';
@@ -16,30 +11,8 @@ import Footer from './components/Footer';
 
 function App() {
 
-  // App.js
-  const [theme, setTheme] = useState('light');
-  const [checked, setChecked] = useState(false);
-
-  const label = "Changer la couleur"
-
-  // The function that toggles between themes
-  const toggleTheme = () => {
-    // if the theme is not light, then set it to dark
-    if (theme === 'light') {
-      setTheme('dark');
-      // otherwise, it should be light
-    } else {
-      setTheme('light');
-    }
-  }
-
-
-
   return (
     <div className="App">
-      <ThemeProvider theme={theme === 'dark' ? lightTheme : darkTheme}>
-        <GlobalStyles />
-        <Button variant="outline-dark" onClick={toggleTheme} className="switch-button">Changer la couleur</Button>{' '}
         <TopRectangle className="topRectangle" />
         <MonParcoursRectangle className="MonParcoursRectangle" />
         <ExperienceProRectangle />
@@ -47,7 +20,6 @@ function App() {
         <MesCompetencesRectangle />
         <CentreInteretRectangle />
         <Footer />
-      </ThemeProvider>
     </div>
   );
 }
